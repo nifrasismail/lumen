584e5a620465cac54b7b97029db2954f297dcc05
mam <mam@miniexchange.com>
2018-12-08 19:39:23 +0000

    die if the .env is not found

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -5,7 +5,7 @@ require_once __DIR__.'/../vendor/autoload.php';
 try {
     (new Dotenv\Dotenv(__DIR__.'/../'))->load();
 } catch (Dotenv\Exception\InvalidPathException $e) {
-    //
+    die('.env path is incorrect');
 }
 
 /*

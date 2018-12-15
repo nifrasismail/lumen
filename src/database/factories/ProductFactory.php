94353f789a0e99347a495dc12588dad3407ee3f2
mam <mamreezaa@gmail.com>
2018-12-15 16:30:31 +0000

    seed dummy data using product factory

---
+++ src/database/factories/ProductFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+$factory->define(App\Product::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'sku' => $faker->unique()->randomNumber(5),
+        'price' => $faker->numberBetween(100,200),
+        'description' => $faker->text(100),
+    ];
+});

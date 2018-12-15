b8e6d4a7655b393889d919173437d5cc7fc79ff9
mam <mamreezaa@gmail.com>
2018-12-15 16:30:03 +0000

    make sku unique

--- src/database/migrations/2018_12_15_104145_create_products_table.php
+++ src/database/migrations/2018_12_15_104145_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('sku');
+            $table->string('sku')->unique();
             $table->string('name');
             $table->decimal('price');
             $table->longText('description');

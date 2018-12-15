82109f7ce700b4b6fd453276b5ff7ac438ba9572
mam <mamreezaa@gmail.com>
2018-12-15 17:46:56 +0000

    create pagination

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -17,10 +17,11 @@ class ProductController extends Controller
     }
 
     public function getAllProducts(){
-        return response()->json(Product::all());
+        return response()->json(Product::paginate(100));
     }
 
     public function getProductById($id){
-        return response()->json(Product::find($id));
+        $res = !empty(Product::find($id)) ? Product::find($id): ['error' => 'Requested Product not found'];
+        return response()->json($res,404);
     }
 }

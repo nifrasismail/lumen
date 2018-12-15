aa75d75fb8c34c490cf02b855a8bd91725c06c2e
mam <mam@miniexchange.com>
2018-12-15 10:38:01 +0000

    create product controller and router

---
+++ src/app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ProductController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function getAllProducts(){
+        return 'apple';
+    }
+}
--- src/routes/web.php
+++ src/routes/web.php
@@ -14,3 +14,7 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->group(['prefix' => 'v1'], function () use ($router) {
+    $router->get('products',  ['uses' => 'ProductController@getAllProducts']);
+});

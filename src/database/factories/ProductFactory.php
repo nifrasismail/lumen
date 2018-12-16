3eaa4f49123315923746f5aeb5435dc48d28c627
mam <mamreezaa@gmail.com>
2018-12-16 18:43:58 +0000

    seed product table

--- src/database/factories/ProductFactory.php
+++ src/database/factories/ProductFactory.php
@@ -6,5 +6,6 @@ $factory->define(App\Product::class, function (Faker\Generator $faker) {
         'sku' => $faker->unique()->randomNumber(5),
         'price' => $faker->numberBetween(100,200),
         'description' => $faker->text(100),
+        'qty' =>$faker->numberBetween(10,20)
     ];
 });

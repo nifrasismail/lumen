4397005ea1ef3295336f1b77316fd8db8c5e3438
mam <mamreezaa@gmail.com>
2019-01-05 12:02:58 +0000

    create new config

---
+++ src/config/resources.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @category    product-service
+ * @package     Product
+ * @author      mAm <mamreezaa@gmail.com>
+ */
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Allowed include resources for sellers
+    |--------------------------------------------------------------------------
+    */
+    'products' => [
+        'images'
+    ],
+
+];
